6e06f8187b36a0910735b676c99cec59ed5edf67
natanguilhermebragion-byte <natanguilhermebragion@gmail.com>
2026-03-30 10:01:25 -0300

    pombo update

--- Atividade grupo04.php
+++ Atividade grupo04.php
@@ -1,43 +1,41 @@
 <?php
 
-function calculaMultaVeiculo($velocidadeReal, $velocidadePermitida, $ehZonaEscolar) {
-   
-    if ($velocidadeReal <= 0 || $velocidadePermitida <= 0) return "erro";
-    if ($velocidadePermitida < 20 || $velocidadePermitida > 120) return "erro";
-    if ($velocidadeReal > 300) return "erro";
+class Grupo04 { 
+    public function calculaMultaVeiculo($velocidadeReal, $velocidadePermitida, $ehZonaEscolar) {
+        
+        if ($velocidadeReal <= 0 || $velocidadePermitida <= 0) return "erro";
+        if ($velocidadePermitida < 20 || $velocidadePermitida > 120) return "erro";
+        if ($velocidadeReal > 300) return "erro";
 
- 
-    if ($velocidadeReal <= $velocidadePermitida) {
-        return "R$ 0,00";
+        if ($velocidadeReal <= $velocidadePermitida) {
+            return "R$ 0,00";
+        }
+
+        $excessoPercentual = (($velocidadeReal - $velocidadePermitida) / $velocidadePermitida) * 100;
+        $valorMulta = 0;
+
+        if ($excessoPercentual <= 20) {
+            $valorMulta = 130.16;
+        } elseif ($excessoPercentual <= 50) {
+            $valorMulta = 195.23;
+        } else {
+            $valorMulta = 880.41;
+        }
+
+        if ($ehZonaEscolar === true) {
+            $valorMulta *= 2;
+        }
+
+        return "R$ " . number_format($valorMulta, 2, ',', '.');
     }
-
-
-    $excessoPercentual = (($velocidadeReal - $velocidadePermitida) / $velocidadePermitida) * 100;
-    $valorMulta = 0;
-
-   
-    if ($excessoPercentual <= 20) {
-        $valorMulta = 130.16;
-    } elseif ($excessoPercentual > 20 && $excessoPercentual <= 50) {
-        $valorMulta = 195.23;
-    } else {
-        $valorMulta = 880.41;
-    }
-
-    
-    if ($ehZonaEscolar === true) {
-        $valorMulta *= 2;
-    }
-
-
-    return "R$ " . number_format($valorMulta, 2, ',', '.');
 }
 
+$sistema = new Grupo04();
 
-echo calculaMultaVeiculo(60, 60, false);  
-echo calculaMultaVeiculo(70, 60, false);  
-echo calculaMultaVeiculo(100, 60, false);  
-echo calculaMultaVeiculo(70, 60, true);   
-echo calculaMultaVeiculo(-10, 60, false); 
+echo $sistema->calculaMultaVeiculo(60, 60, false) . "<br>";
+echo $sistema->calculaMultaVeiculo(70, 60, false) . "<br>";
+echo $sistema->calculaMultaVeiculo(100, 60, false) . "<br>";
+echo $sistema->calculaMultaVeiculo(70, 60, true) . "<br>";
+echo $sistema->calculaMultaVeiculo(-10, 60, false) . "<br>";
 
 ?>
